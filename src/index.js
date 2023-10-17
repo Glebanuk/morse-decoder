c2465675eae944ad3dfaa0a4508ba0c69fde772f
glebanuk <glebbong12387@gmail.com>
2023-10-17 15:39:06 +0300

    feat: add decoded Morse Code

--- src/index.js
+++ src/index.js
@@ -38,8 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let symbols = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        symbols.push(expr.slice(i, i + 10));
+    }
+
+    let decodedSymb = symbols.map(function (symbol) {
+        return decodedSymbol(symbol);
+    });
+
+    return decodedSymb.join("");
+
+    function decodedSymbol(symbol) {
+        if (symbol === '**********') {
+            
+            return ' ';
+        }
+
+        
+        symbol = symbol.replace(/^0+/, '');
+
+        let decodedSymbol = '';
+        
+        for (let i = 0; i < symbol.length; i += 2) {
+            const part = symbol.slice(i, i + 2);
+            if (part === '10') {
+                decodedSymbol += '.';
+            } else if (part === '11') {
+                decodedSymbol += '-';
+            }
+        }
+
+     
+        return MORSE_TABLE[decodedSymbol];
+    }
 }
+
+
+
 
 module.exports = {
     decode
